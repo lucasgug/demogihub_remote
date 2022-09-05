5f9be4867447e304fa87100d30d0d83ca66ad6b5
lucas nicolas gugliuzza <lucasgugliuzza@outlook.com>
2022-09-05 00:37:14 -0300

    Initial Commit

---
+++ demogihub/demogihubApp.swift
@@ -0,0 +1,17 @@
+//
+//  demogihubApp.swift
+//  demogihub
+//
+//  Created by Lucas Gugliuzza on 05/09/2022.
+//
+
+import SwiftUI
+
+@main
+struct demogihubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
