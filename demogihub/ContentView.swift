02d11f10ac58390ea2d33c1bec4b90b8801c2afc
lucas nicolas gugliuzza <lucasgugliuzza@outlook.com>
2022-09-05 00:41:59 -0300

    CAMBIO EN ContentView

--- demogihub/ContentView.swift
+++ demogihub/ContentView.swift
@@ -1,21 +1,23 @@
-//
-//  ContentView.swift
-//  demogihub
-//
-//  Created by Lucas Gugliuzza on 05/09/2022.
-//
+        //
+        //  ContentView.swift
+        //  demogihub
+        //
+        //  Created by Lucas Gugliuzza on 05/09/2022.
+        //
 
-import SwiftUI
+        import SwiftUI
 
-struct ContentView: View {
-    var body: some View {
-        Text("Hello, world!")
-            .padding()
-    }
-}
+        struct ContentView: View {
+            
+            // MARK: BODY
+            var body: some View {
+                Text("This is a test ")
+                    .padding()
+            }
+        }
 
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
-    }
-}
+        struct ContentView_Previews: PreviewProvider {
+            static var previews: some View {
+                ContentView()
+            }
+        }
